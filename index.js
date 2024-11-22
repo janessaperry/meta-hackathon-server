07245c7d44099d3f0bd751cb5062ad7e2fe252b4
sbudhia <sanbud@LEGIONOVO.>
2024-11-21 18:51:44 -0600

    Adding initial GET call and sample data structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-
+import fs from 'fs';
 
 let app = express();
 app.use(cors());
@@ -17,9 +17,47 @@ app.use((req,res,next) => {
     }
     next();
 })
-const port = 8080;//process.env.PORT 
+const port = 8080;
 
 app.listen(port,()=>{
     console.log("Server is listening at port:", port)
 })
 
+//this can be completely changed based on what the front-end needs
+app.get("/posts/:id", (req,res)=>{
+    let dataId = req.params.id;
+    let data = JSON.parse(fs.readFileSync(`./data/posts/${dataId}.json`))
+    res.json(data)
+})
+
+app.get("/profiles/:id", (req,res)=>{
+    let dataId = req.params.id;
+    let data = JSON.parse(fs.readFileSync(`./data/profiles/${dataId}.json`))
+    res.json(data)
+})
+
+
+/*
+
+data structure:
+
+pageData{
+    "type":"posts"
+    "images":[imgPath,imgPath,imgPath,...]
+}
+
+pageData{
+    "type":"profiles"
+    categories:[category,category]
+    profiles:[profileObj,profileObj,profileObj]
+    
+}
+
+profileObj
+    {
+    profileName:____,
+    profilePhoto:path,
+    profilePosts:[imgPath,imgPath,imgPath]
+    }
+
+*/
